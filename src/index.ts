9704b2658f67cea527d8b355bb39c62aae0bafcc
brx-nbif <blondeau.nbif@mail.ru>
2025-04-15 11:57:46 +0100

    feat: update default response to include JSON data
    The default response was changed from a simple 'Hello World!' message to a JSON object containing the name 'Fariol Blondeau'. This modification provides more structured and meaningful data in the response, which can be useful for client-side processing or debugging.

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		return new Response(JSON.stringify({ name: 'Fariol Blondeau' }));
 	},
 } satisfies ExportedHandler<Env>;
