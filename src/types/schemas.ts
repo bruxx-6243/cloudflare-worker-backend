f10a4362380eac81f7e05a4bdaa9697a347f6023
brx-nbif <blondeau.nbif@mail.ru>
2025-04-18 14:52:14 +0100

    refactor(schemas): extract password validation into reusable schema
    The password validation logic was duplicated in both `loginSchema` and `registerSchema`. To improve maintainability and reduce redundancy, the password validation rules were extracted into a reusable `PasswordSchema` and referenced in both schemas.

--- src/types/schemas.ts
+++ src/types/schemas.ts
@@ -4,26 +4,22 @@ export const STATUS_ENUM = z.enum(['ACTIVE', 'INACTIVE', 'DELETED'], {
 	errorMap: () => ({ message: 'Status must be ACTIVE, INACTIVE, or DELETED' }),
 });
 
+export const PasswordSchema = z
+	.string()
+	.min(6, { message: 'Password must be at least 6 characters long' })
+	.regex(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])/, {
+		message: 'Password must contain at least one capital letter, one number, and one special character',
+	});
+
 export const loginSchema = z.object({
 	email: z.string().email(),
-	password: z
-		.string()
-		.min(6, { message: 'Password must be at least 6 characters long' })
-		.regex(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])/, {
-			message: 'Password must contain at least one capital letter, one number, and one special character',
-		}),
+	password: PasswordSchema,
 });
 
 export const registerSchema = z.object({
+	password: PasswordSchema,
 	userName: z.string().min(1, { message: 'Username is required' }).max(255, { message: 'Username must not exceed 255 characters' }),
 	firstName: z.string().min(1, { message: 'First name is required' }).max(255, { message: 'First name must not exceed 255 characters' }),
 	lastName: z.string().min(1, { message: 'Last name is required' }).max(255, { message: 'Last name must not exceed 255 characters' }),
-	password: z
-		.string()
-		.min(6, { message: 'Password must be at least 6 characters long' })
-		.max(255, { message: 'Password must not exceed 255 characters' })
-		.regex(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])/, {
-			message: 'Password must contain at least one capital letter, one number, and one special character',
-		}),
 	email: z.string().email({ message: 'Invalid email address' }).max(255, { message: 'Email must not exceed 255 characters' }),
 });
