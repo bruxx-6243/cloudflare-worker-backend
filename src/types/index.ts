99c3261648c4ff0022c8689b3ced573e425f5207
brx-nbif <blondeau.nbif@mail.ru>
2025-04-20 14:48:09 +0100

    feat(auth): add email notification for login and registration
    Introduce email notifications using Courier service for both login and registration events. Added new environment variables for template IDs and Courier auth token. Updated type definitions and created a new email service module to handle email sending logic.

--- src/types/index.ts
+++ src/types/index.ts
@@ -33,3 +33,12 @@ export type LoginType = z.infer<typeof loginSchema>;
 export type StatusType = (typeof STATUS_ENUM._def.values)[number];
 export type RegisterType = Omit<typeof schema.usersTable.$inferInsert, 'id' | 'createdAt' | 'updatedAt'>;
 export type User = typeof schema.usersTable.$inferSelect;
+
+export type EmailPayload = {
+	to: string;
+	body?: string;
+	name?: string;
+	email?: string;
+	subject?: string;
+	template: string;
+};
